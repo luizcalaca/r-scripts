d37959466e9edae8411a9cb8ec7d2c374b0feadf
LUIZ FERNANDO CALAçA SILVA <luiz.calaca@al.go.leg.br>
2017-08-23 11:19:09 -0300

    Correction query, fields and text plot

--- sqldf_plot.R
+++ sqldf_plot.R
@@ -1,17 +1,16 @@
 library(sqldf)
 
-#create dataframe
-dataset <- data.frame(pessoa_id, data_desligamento, ativo, sexo)
+#create dataframe #Selecting fields on Power Bi
+dataset <- data.frame(pessoa_id, data_desligamento, ativo)
 
 #remove duplicated rows
 dataset <- unique(dataset)
 
 sub_dataset <- sqldf(
   'SELECT count(pessoa_id) FROM dataset
-  WHERE data_desligamento is null OR data_desligamento > date()
-  GROUP BY sexo')
+  WHERE data_desligamento is null OR data_desligamento > date() AND ativo is true')
 
 plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
-text(x = 0.5, y = 0.5, paste("Total de Funcionários\n\n",
+text(x = 0.5, y = 0.5, paste("Total of Employees\n\n",
                              nrow(sub_dataset)), 
      cex = 1.6, col = "black")
