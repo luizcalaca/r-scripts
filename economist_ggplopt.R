978f4bf76aac5d1b03d3379f5b2f1d187466fb1a
Luiz Fernando Calaça Silva <luizcalaca@gmail.com>
2017-08-31 11:59:03 -0300

    Create economist_ggplopt.R

---
+++ economist_ggplopt.R
@@ -0,0 +1,32 @@
+library(ggplot2)
+library(data.table)
+library(ggthemes)
+
+df <- fread('economist.csv',drop=1)
+pl <- ggplot(df,aes(x=CPI,y=HDI,color=Region)) + geom_point(size=4,shape=1)
+pl <- pl + geom_smooth(aes(group=1))
+
+pl2 <- pl + geom_smooth(aes(group=1),method ='lm',formula = y~log(x),se=FALSE,color='red')
+pl2 + geom_text(aes(label=Country))
+
+pointsToLabel <- c("Russia", "Venezuela", "Iraq", "Myanmar", "Sudan",
+                   "Afghanistan", "Congo", "Greece", "Argentina", "Brazil",
+                   "India", "Italy", "China", "South Africa", "Spane",
+                   "Botswana", "Cape Verde", "Bhutan", "Rwanda", "France",
+                   "United States", "Germany", "Britain", "Barbados", "Norway", "Japan",
+                   "New Zealand", "Singapore")
+
+pl3 <- pl2 + geom_text(aes(label = Country), color = "gray20", 
+                       data = subset(df, Country %in% pointsToLabel),check_overlap = TRUE)
+
+pl4 <- pl3 + theme_bw() 
+pl5 <- pl4 + scale_x_continuous(name = "Corruption Perceptions Index, 2011 (10=least corrupt)",
+                                limits = c(.9, 10.5),breaks=1:10) 
+
+pl6 <- pl5 + scale_y_continuous(name = "Human Development Index, 2011 (1=Best)",
+                                limits = c(0.2, 1.0))
+
+pl6 <- pl6 + ggtitle("Corruption and Human development")
+
+pl6 + theme_economist_white()
+print(pl6)
